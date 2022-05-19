ebceb8a7964ab8ab5834263b0e3dc7ad0effe4e7
rishistrive <rishi.strive@gmail.com>
2022-05-19 11:18:28 +0530

    changed database to mysql, fix issues in charts, shipment , view

--- shipmenttracker/settings.py
+++ shipmenttracker/settings.py
@@ -88,12 +88,24 @@ MEDIA_URL = "/media/"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'shipments',
+        'USER': 'root',
+        'PASSWORD': 'Root@123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
