7ad6cea1e986c21e3f416f88a39dcaffc5c0c18a
developer <rishi.strive@gmail.com>
2022-05-24 14:58:36 +0530

    fix bug average amt

--- stats/views.py
+++ stats/views.py
@@ -11,18 +11,16 @@ def stats(request):
     supplier = request.user.is_supplier
     if not supplier:
         shipments = request.user.customer.all().select_related('product_name')
-        shipment_count = shipments.count()
         for shipment in shipments:
             labels.append(shipment.product_name.name)
             data.append(shipment.quantity)
-        avg=Shipment.objects.aggregate(Avg('order_amount'))
+        avg=request.user.customer.all().aggregate(Avg('order_amount'))
     else:
         shipments = request.user.supplier.all().select_related('product_name')
-        shipment_count = shipments.count()
         for shipment in shipments:
             labels.append(shipment.product_name.name)
             data.append(shipment.order_amount)
-        avg=Shipment.objects.aggregate(Avg('order_amount'))
+        avg=request.user.supplier.all().aggregate(Avg('order_amount'))
     return render(
         request,
         "stats/stats.html",
